069d10bb2b3e37cbdc46b7f9d3796abc0747b5e9
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-08-11 23:06:13 +0330

    add username fetcher for dash

---
+++ public_html/dashboard/dashboard.js
@@ -0,0 +1,24 @@
+//fetch username using token
+const url = "http://127.0.0.1:8080/dashboard";
+const username = document.querySelector("#username");
+function getToken() {
+  return document.cookie.split("=")[1];
+}
+
+async function setUserName() {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      username.textContent = data.data.username;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+setUserName();
