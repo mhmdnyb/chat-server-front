e258f6bbb647a07d079d546e7fe62f5862468a92
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-08-14 19:54:39 +0330

    redefine userinfo fetcher

--- public_html/register_modal/register.js
+++ public_html/register_modal/register.js
@@ -32,9 +32,10 @@ cb1.addEventListener("input", (e) => {
 //Return of userInfo
 function userInfo() {
   const form = document.querySelector("#form");
+
   const credentials = {
-    username: form.children[1].value,
-    password: form.children[2].value,
+    username: form.children[1].children[0].value,
+    password: form.children[2].children[0].value,
     birthDate: `${form.children[4].children[0].value}-${form.children[4].children[1].value}-${form.children[4].children[2].value}`,
   };
 
