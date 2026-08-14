d31782af844a3c22dd86b1a44a3f787e963d3c49
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-08-14 19:55:37 +0330

    do whole change password logic

--- public_html/dashboard/password/password.js
+++ public_html/dashboard/password/password.js
@@ -2,6 +2,29 @@ const form = document.querySelector("#form");
 const change = document.querySelector("#change");
 const url = "http://127.0.0.1:8080/dashboard/change-password";
 
+function redirection(url) {
+  window.location = url;
+}
+
+function showError(message, time = 3500, sound = "error", redirect = false) {
+  const msg = document.querySelector("#error-msg");
+  const modal = document.querySelector("#modal1");
+  const sounds = { confirm: "/audio/confirm.mp3", error: "/audio/error.mp3" };
+  const audio = new Audio(sounds[sound]);
+  audio.play();
+
+  msg.textContent = message;
+  modal.classList.add("pop-up");
+  modal.classList.replace("hidden", "flex");
+  setTimeout(() => {
+    modal.classList.replace("flex", "hidden");
+    modal.classList.remove("pop-up");
+    if (redirect == true) {
+      redirection("/public_html/dashboard/dashboard.html");
+    }
+  }, time);
+}
+
 function getToken() {
   return document.cookie.split("=")[1];
 }
@@ -19,9 +42,11 @@ async function sendData() {
         newPassword: `${form.children[1].children[1].children[0].value}`,
       }),
     });
-    const data = await response.text();
-    if (response.ok) {
-      console.log(data);
+    const data = await response.json();
+    if (data.status === "OK") {
+      showError(`Done ${data.data.username}`, 3500, "confirm", true);
+    } else {
+      showError(`${data.data.message}`);
     }
   } catch (error) {
     console.log(error);
@@ -30,5 +55,12 @@ async function sendData() {
 
 change.addEventListener("click", (e) => {
   e.preventDefault();
-  sendData();
+  if (
+    form.children[1].children[2].children[0].value ===
+    form.children[1].children[1].children[0].value
+  ) {
+    sendData();
+  } else {
+    showError("Password Mismatch");
+  }
 });
