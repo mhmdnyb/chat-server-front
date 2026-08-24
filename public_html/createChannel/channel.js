1f2de97a2277237e52b188f91d662c1c2a4c09d1
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-08-24 23:31:10 +0330

    add create channel logic

--- public_html/createChannel/channel.js
+++ public_html/createChannel/channel.js
@@ -1,15 +1,15 @@
-import { stringToHTML } from "../index.js";
+import { stringToHTML, getToken } from "../index.js";
 import { settingPage } from "../home.js";
-let btn;
+import { showError } from "../index.js";
+const CREATE_URL = `http://127.0.0.1:8080/home/create-channel`;
 const channelModalString = `<form id="channelform"
-      action="http://127.0.0.1:8080/home/create-group"
       method="post"
       class="h-full w-full flex flex-col items-center justify-center gap-2 relative"
     >
       <div class="w-65">
         <input
           type="text"
-          name="channelName"
+          name="name"
           id="channelName"
           class="w-full bg-[#262626] border border-white/40 text-tenpercent placeholder-gray-600 rounded-md px-4 py-2.5 focus:outline-none opacity-85 focus:ring-1 focus:ring-yellow-300 focus:opacity-100 transition-all duration-300"
           placeholder="Channel name"
@@ -18,7 +18,7 @@ const channelModalString = `<form id="channelform"
       <div class="w-65">
         <input
           type="text"
-          name="username"
+          name="publicId"
           id="username"
           class="w-full bg-[#262626] border border-white/40 text-tenpercent placeholder-gray-600 rounded-md px-4 py-2.5 focus:outline-none opacity-85 focus:ring-1 focus:ring-yellow-300 focus:opacity-100 transition-all duration-300"
           placeholder="Channel username"
@@ -34,14 +34,14 @@ const channelModalString = `<form id="channelform"
         />
       </div>
       <label for="private" class="group cursor-pointer">
-        <input type="checkbox" name="private" id="private" class="sr-only" />
+        <input type="checkbox" name="privateChannel" id="private" class="sr-only" />
         <div class="flex items-center justify-between w-65">
           <p>Private Channel?</p>
           <div
             class="w-18 h-8 rounded-full bg-white relative group-has-checked:bg-sky-500 transition-all"
           >
             <div
-              class="size-7 bg-black rounded-full absolute bottom-0.5 group-has-checked:translate-x-11 transition-all"
+              class="size-7 bg-black rounded-full absolute bottom-0.5 group-has-checked:translate-x-11 transition-all group-has-checked:bg-white"
             ></div>
           </div>
         </div>
@@ -54,6 +54,28 @@ const channelModalString = `<form id="channelform"
         Create
       </button>
     </form>`;
+
+async function createChannel(data) {
+  console.log(data);
+  try {
+    const response = await fetch(CREATE_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (response.ok) {
+      const body = await response.json();
+      showError("Success!", "confirm");
+    } else {
+      showError("Error!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 export function channelModal() {
   settingPage.innerHTML = channelModalString;
   const form = document.querySelector("#channelform");
@@ -61,6 +83,11 @@ export function channelModal() {
     e.preventDefault();
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
-    console.log(data);
+    if (data.privateChannel) {
+      const newData = { ...data, privateChannel: true };
+      createChannel(newData);
+    } else {
+      createChannel(data);
+    }
   });
 }
