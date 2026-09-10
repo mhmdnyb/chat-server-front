b9e1ee7a301d979c8d1c5cc7c6c9d12bb5b4a5ae
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-09-10 12:22:04 +0330

    fix showpassword and progressbar

--- chat-project/src/pages/RegisterPage/RegisterPage.jsx
+++ chat-project/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,14 +1,75 @@
 import "./RegisterPage.css";
 import Header from "../../components/Header/Header";
+import { useState } from "react";
+import { useRef } from "react";
+
+class ProgressBar {
+  constructor() {
+    this.username = false;
+    this.password = false;
+    this.tos = false;
+    this.width = 0;
+  }
+  validateUserName(formRef, progressRef) {
+    const usernameValidation = /^[a-zA-Z0-9_]{3,16}$/;
+    const username = formRef.children[2].children[0].value;
+    if (usernameValidation.test(username) && !this.username) {
+      this.width += 25;
+      progressRef.setAttribute("style", `width:${this.width}%`);
+      this.username = true;
+    } else if (!usernameValidation.test(username) && this.username) {
+      this.width -= 25;
+      progressRef.setAttribute("style", `width:${this.width}%`);
+      this.username = false;
+    }
+  }
+  validatePassword(formRef, progressRef) {
+    const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    const password = formRef.children[3].children[0].value;
+    if (passwordValidation.test(password) && !this.password) {
+      this.width += 25;
+      progressRef.setAttribute("style", `width:${this.width}%`);
+      this.password = true;
+    } else if (!passwordValidation.test(password) && this.password) {
+      this.width -= 25;
+      progressRef.setAttribute("style", `width:${this.width}%`);
+      this.password = false;
+    }
+  }
+  validateTOS(tosRef, progressRef) {
+    if (tosRef.checked) {
+      this.width += 50;
+      progressRef.setAttribute("style", `width:${this.width}%`);
+    } else {
+      this.width -= 50;
+      progressRef.setAttribute("style", `width:${this.width}%`);
+    }
+  }
+}
 function RegisterPage() {
+  const [type, setType] = useState("password");
+  const formRef = useRef();
+  const tosRef = useRef();
+  const progressRef = useRef();
+
   {
     document.querySelector("body").style.backgroundColor = "#6366f1";
   }
+  const temp = new ProgressBar();
+
   return (
     <>
       <Header></Header>
       <section className="w-screen h-screen flex justify-center items-center">
         <form
+          ref={formRef}
+          onBlur={(e) => {
+            if (e.target.id === "username") {
+              temp.validateUserName(formRef.current, progressRef.current);
+            } else if (e.target.id === "password") {
+              temp.validatePassword(formRef.current, progressRef.current);
+            }
+          }}
           id="form"
           action="index.php"
           method="POST"
@@ -16,6 +77,7 @@ function RegisterPage() {
         >
           <div className="bg-linear-to-bl from-white/50 to-white/35 border border-white/25 shadow-lg shadow-black/30 rounded-full w-full h-3.5 -mt-1.5 backdrop-blur-2xl absolute bottom-0 left-0">
             <div
+              ref={progressRef}
               id="progress"
               style={{ width: 0 }}
               className="h-full bg-linear-to-r from-[#b1afce] to-[#3f39f1] rounded-full transition-all duration-300"
@@ -38,7 +100,7 @@ function RegisterPage() {
           <div>
             <input
               name="password"
-              type="password"
+              type={type}
               id="password"
               placeholder="Password"
               className="border-2 border-tenpercent rounded-lg pl-3 focus:outline-0 focus:ring-2 focus:ring-white/50 w-full focus:transition-all duration-50 min-h-8 py-0.5"
@@ -48,35 +110,90 @@ function RegisterPage() {
           <div className="border-2 border-tenpercent rounded-lg pl-3 flex items-center has-focus:outline-0 has-focus:ring-2 has-focus:ring-white/50 min-h-8 has-focus:transtion-all py-0.5">
             <input
               className="focus:outline-0 w-full"
-              type="password"
+              type={type}
               name="confirmPassword"
               id="confirmPassword"
               placeholder="Confirm password"
             />
-            <svg
-              id="show-pw"
-              className="inline-block ml-auto mr-1 cursor-pointer"
-              width="25px"
-              height="25px"
-              viewBox="0 0 25 25"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M5.20513 12.5C6.66296 14.7936 8.9567 16.9 12.5 16.9C16.0433 16.9 18.3371 14.7936 19.7949 12.5C18.3371 10.2064 16.0433 8.1 12.5 8.1C8.9567 8.1 6.66296 10.2064 5.20513 12.5ZM3.98551 12.1913C5.53974 9.60093 8.20179 6.9 12.5 6.9C16.7982 6.9 19.4603 9.60093 21.0145 12.1913L21.1997 12.5L21.0145 12.8087C19.4603 15.3991 16.7982 18.1 12.5 18.1C8.20179 18.1 5.53974 15.3991 3.98551 12.8087L3.80029 12.5L3.98551 12.1913ZM12.5 9.4C10.7879 9.4 9.4 10.7879 9.4 12.5C9.4 14.2121 10.7879 15.6 12.5 15.6C14.2121 15.6 15.6 14.2121 15.6 12.5C15.6 10.7879 14.2121 9.4 12.5 9.4Z"
-                  fill="#121923"
-                ></path>
-              </g>
-            </svg>
+            {type === "password" ? (
+              <svg
+                onClick={() => {
+                  if (type === "password") {
+                    setType("text");
+                  } else {
+                    setType("password");
+                  }
+                }}
+                id="show-pw"
+                className="inline-block ml-auto mr-1 cursor-pointer"
+                width="24px"
+                height="24px"
+                viewBox="0 0 25 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M5.20513 12.5C6.66296 14.7936 8.9567 16.9 12.5 16.9C16.0433 16.9 18.3371 14.7936 19.7949 12.5C18.3371 10.2064 16.0433 8.1 12.5 8.1C8.9567 8.1 6.66296 10.2064 5.20513 12.5ZM3.98551 12.1913C5.53974 9.60093 8.20179 6.9 12.5 6.9C16.7982 6.9 19.4603 9.60093 21.0145 12.1913L21.1997 12.5L21.0145 12.8087C19.4603 15.3991 16.7982 18.1 12.5 18.1C8.20179 18.1 5.53974 15.3991 3.98551 12.8087L3.80029 12.5L3.98551 12.1913ZM12.5 9.4C10.7879 9.4 9.4 10.7879 9.4 12.5C9.4 14.2121 10.7879 15.6 12.5 15.6C14.2121 15.6 15.6 14.2121 15.6 12.5C15.6 10.7879 14.2121 9.4 12.5 9.4Z"
+                    fill="#121923"
+                  ></path>
+                </g>
+              </svg>
+            ) : (
+              <svg
+                className="inline-block ml-auto mr-1 cursor-pointer"
+                onClick={() => {
+                  if (type === "password") {
+                    setType("text");
+                  } else {
+                    setType("password");
+                  }
+                }}
+                width="24px"
+                height="24px"
+                viewBox="-2 -2 28 28"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M2 2L22 22"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>{" "}
+                  <path
+                    d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>{" "}
+                  <path
+                    d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>{" "}
+                </g>
+              </svg>
+            )}
           </div>
 
           <div className="flex justify-around items-center mt-1.5">
@@ -110,7 +227,15 @@ function RegisterPage() {
           </div>
 
           <div className="flex justify-center items-center gap-x-1.5 mt-1.5">
-            <input type="checkbox" name="tos" id="tos" />
+            <input
+              type="checkbox"
+              name="tos"
+              id="tos"
+              ref={tosRef}
+              onInput={() => {
+                temp.validateTOS(tosRef.current, progressRef.current);
+              }}
+            />
             <label htmlFor="tos" className="text-sm">
               By Registering You Agree With Our
               <a
