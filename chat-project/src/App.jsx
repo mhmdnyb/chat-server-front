2dcf4b8e3842ce0c5f85ae6b6e65510fb3fda0bc
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-09-11 17:38:11 +0330

    add dashboard page

--- chat-project/src/App.jsx
+++ chat-project/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import "./App.css";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route index element={<RegisterPage></RegisterPage>}></Route>
       <Route path="/login" element={<LoginPage></LoginPage>}></Route>
       <Route path="/home" element={<HomePage></HomePage>}></Route>
+      <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
     </Routes>
   );
 }
--- chat-project/src/pages/Dashboard/Dashboard.jsx
+++ chat-project/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,63 @@
 import "./Dashboard.css";
+import UserProfileImage from "../../assets/placeholder.webp";
 function Dashboard() {
-  return <></>;
+  {
+    document.querySelector("body").style.backgroundColor = "#6366f1";
+  }
+  return (
+    <section className="w-screen h-screen flex justify-center items-center saira-condensed-medium">
+      <div className="bg-linear-to-bl from-white/25 to-white/15 backdrop-blur-2xl border border-white/40 shadow-xl px-4 py-2 rounded-xl space-y-2.5">
+        <div className="rounded-full flex items-center justify-evenly">
+          <img
+            className="rounded-full outline-2 outline-black/30 w-15 h-15 sm:w-25 sm:h-25"
+            src={UserProfileImage}
+            width="100"
+            height="100"
+            alt="profilePicture"
+          />
+          <input
+            className="file:mx-4 file:mr-6 file:bg-thirtypercent file:py-1.5 file:px-3 file:rounded-full hover:file:opacity-90 file:transition-opacity"
+            type="file"
+            name="pfp"
+            id="pfp"
+            accept="image/jpeg, image/png, image/webp"
+          />
+        </div>
+        <h2 className="text-tenpercent text-shadow-sm text-shadow-black/20 text-center text-xl saira-condensed-bold border-b border-b-black/50 animate-pulse">
+          Your Account
+        </h2>
+
+        <div className="flex items-center justify-between text-lg">
+          <h3>Username</h3>
+          <div>
+            <h3 className="inline mr-1.5 text-white" id="username"></h3>
+            <button
+              // onclick="changeInfo()"
+              id="changeUser"
+              className="bg-thirtypercent px-5 rounded-2xl text-black/70 cursor-pointer"
+            >
+              Change
+            </button>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between text-lg">
+          <h3>Password</h3>
+
+          <a
+            href="./password/password.html"
+            target="_blank"
+            className="bg-thirtypercent px-5 rounded-2xl text-black/70 cursor-pointer"
+          >
+            Change
+          </a>
+        </div>
+        <div className="flex items-center justify-between text-lg">
+          <h3>UID</h3>
+          <h3 className="inline mr-1.5 text-white" id="uid"></h3>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Dashboard;
