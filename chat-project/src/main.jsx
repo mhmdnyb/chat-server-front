5564c5655c2cbb6364f921bfc6fd2eb9d7493718
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-09-22 19:31:48 +0330

    add react-toast

--- chat-project/src/main.jsx
+++ chat-project/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { Toaster } from "react-hot-toast";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -6,5 +6,6 @@ import { BrowserRouter } from "react-router";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Toaster position="top-center"></Toaster>
   </BrowserRouter>,
 );
